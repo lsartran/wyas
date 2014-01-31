d3494f034b09c4ff6e4820ed865f7e720f92dcd8
Laurent Sartran <lsartran@gmail.com>
2014-01-31 16:39:49 +0100

    Add error handling infrastructure

--- parser.hs
+++ parser.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+--import Control.Monad.Error
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -9,7 +10,24 @@ data LispVal =  Atom String
             |   String String
             |   Bool Bool
 
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
 instance Show LispVal where show = showVal
+instance Show LispError where show = showError
+
+instance Error LispError where
+     noMsg = Default "An error has occurred"
+     strMsg = Default
+
+type ThrowsError = Either LispError
+
+trapError action = catchError action (return . show)
 
 charToString :: Char -> String
 charToString = flip (:) []
@@ -85,6 +103,14 @@ showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+showError :: LispError -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found) = "Expected " ++ show expected ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found " ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
 
 eval :: LispVal -> LispVal
 eval val@(String _) = val
