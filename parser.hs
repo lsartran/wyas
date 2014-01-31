776c8e17ebd6f9566a98e394b0ff82e3a8dc5bfc
Laurent Sartran <lsartran@gmail.com>
2014-01-31 13:13:45 +0100

    Eval

--- parser.hs
+++ parser.hs
@@ -69,10 +69,10 @@ parseQuoted = do
     x <- parseExpr
     return $ List [Atom "quote", x]
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match:" ++ show err
-    Right val -> "Found value:" ++ show val
+    Left err -> String $ "No match:" ++ show err
+    Right val -> val
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
@@ -86,7 +86,11 @@ showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+
 main :: IO ()
-main = do
-    args <- getArgs
-    putStrLn $ readExpr $ args !! 0
+main = getArgs >>= print . eval . readExpr . head
